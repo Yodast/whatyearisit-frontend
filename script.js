94d521df6a0bb8986bb5bdf784dcf556cc2ca692
jeremyleclere <leclere.jerem@gmail.com>
2024-11-01 14:26:38 +0100

    init du front

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch('https://whatyearisit-backend-psi-six.vercel.app/year')
+	.then(response => response.json())
+	.then(data => {
+        console.log(data);
+        document.querySelector('#year').textContent += data.Year
+
+		
+	});
